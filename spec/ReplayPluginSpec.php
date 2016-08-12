1fa3e25ccd77b7f01b943d646b291127d896d1a6
Guillem CANAL (ElevenLabs) <guillem.canal.ext@francetv.fr>
2016-08-12 18:19:16 +0200

    test: complete test suite

--- spec/ReplayPluginSpec.php
+++ spec/ReplayPluginSpec.php
@@ -5,7 +5,6 @@ use Http\Client\Common\Plugin;
 use Http\Message\StreamFactory;
 use Http\Promise\FulfilledPromise;
 use PhpSpec\ObjectBehavior;
-use Prophecy\Argument;
 use Psr\Cache\CacheItemInterface;
 use Psr\Cache\CacheItemPoolInterface;
 use Psr\Http\Message\RequestInterface;
@@ -65,35 +64,32 @@ class ReplayPluginSpec extends ObjectBehavior
     }
 
 
-    public function it_record_an_http_response(CacheItemPoolInterface $pool, CacheItemInterface $item, RequestInterface $request, ResponseInterface $response, StreamInterface $requestStream, StreamInterface $responseStream)
+    public function it_record_an_http_response(CacheItemPoolInterface $pool, CacheItemInterface $item, RequestInterface $request, ResponseInterface $response, StreamInterface $stream)
     {
-        $requestHttpBody = 'body';
-        $requestStream->__toString()->willReturn($requestHttpBody);
+        $httpBody = 'body';
 
-        $request->getMethod()->willReturn('GET');
+        $request->getMethod()->willReturn('POST');
         $request->getUri()->willReturn('/');
         $request->getHeaders()->willReturn(['foo' => ['bar']]);
-        $request->getBody()->willReturn($requestStream);
+        $request->getBody()->willReturn('body');
 
-        $responseHttpBody = '{"baz": "bat"}';
-        $responseStream->__toString()->willReturn($responseHttpBody);
-        $responseStream->isSeekable()->willReturn(true);
-        $responseStream->rewind()->shouldBeCalled();
+        $stream->__toString()->willReturn($httpBody);
+        $stream->isSeekable()->willReturn(true);
+        $stream->rewind()->shouldBeCalled();
 
         $response->getStatusCode()->willReturn(200);
-        $response->getReasonPhrase()->willReturn('Ok');
-        $response->getHeaders()->willReturn(['foo' => ['bar']]);
-        $response->getBody()->willReturn($responseStream);
+        $response->getReasonPhrase()->willReturn('OK');
+        $response->getHeaders()->willReturn(['bar' => ['baz']]);
+        $response->getBody()->willReturn($stream);
 
-        $pool->getItem(Argument::any())->willReturn($item)->shouldBeCalled();
+        $pool->getItem('test-bucket-590d21bf0f0be63ba8c0cc67fbde8b2cc02c4314')->willReturn($item)->shouldBeCalled();
         $pool->save($item)->shouldBeCalled();
 
-        $item->isHit()->willReturn(false)->shouldBeCalled();
+        $item->isHit()->willReturn(false);
         $item->set([
             'response' => $response->getWrappedObject(),
-            'body' => $responseHttpBody
+            'body' => $httpBody
         ])->shouldBeCalled();
-
 
         $next = function () use ($response) {
             return new FulfilledPromise($response->getWrappedObject());
@@ -101,8 +97,38 @@ class ReplayPluginSpec extends ObjectBehavior
 
         $this->enableRecorder();
         $this->setBucket('test-bucket');
+
         $this->handleRequest($request, $next, function (){});
     }
 
+    function it_replay_a_recorded_response(CacheItemPoolInterface $pool, CacheItemInterface $item, RequestInterface $request, ResponseInterface $response, StreamInterface $stream, StreamFactory $streamFactory)
+    {
+        $httpBody = 'body';
+
+        $request->getMethod()->willReturn('GET');
+        $request->getUri()->willReturn('/');
+        $request->getHeaders()->willReturn(['foo' => ['bar']]);
+        $request->getBody()->willReturn('');
+
+        $pool->getItem('test-bucket-0052ccfb7378a0478e9013720c1ba606bcf36131')->shouldBeCalled()->willReturn($item);
+        $item->isHit()->willReturn(true);
+        $item->get()->willReturn([
+            'response' => $response,
+            'body' => $httpBody
+        ])->shouldBeCalled();
+
+        // Make sure we add back the body
+        $response->withBody($stream)->willReturn($response)->shouldBeCalled();
+        $streamFactory->createStream($httpBody)->shouldBeCalled()->willReturn($stream);
+
+        $next = function (RequestInterface $request) use ($response) {
+            return new FulfilledPromise($response->getWrappedObject());
+        };
+
+        $this->enableRecorder();
+        $this->setBucket('test-bucket');
+
+        $this->handleRequest($request, $next, function () {});
+    }
 
 }
